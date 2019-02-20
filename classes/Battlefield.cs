ec4b6b3f407dbc13d0c2e806700486b38e7d3e36
Mike <babichmichail@gmail.com>
2019-02-20 18:27:24 -0400

    s,,d;ms

--- classes/Battlefield.cs
+++ classes/Battlefield.cs
@@ -11,6 +11,11 @@ namespace classes
         List<Ship> ships = new List<Ship>();
         List<Coords> shots = new List<Coords>();
 
+        public Battlefield()
+        {
+            ships.Add(new Ship(2, 1, 1, Direction.Horisontal));
+            ships.Add(new Ship(3, 2, , Direction.Vertical));
+        }
 
         public bool addShip(Ship s)
         {
@@ -18,6 +23,22 @@ namespace classes
             return true;
         }
 
+        public HitResponse Hit(Coords c) 
+        {
+            HitResponse res = HitResponse.Miss;
+            foreach(Ship s in ships)
+            {
+                res = s.CheckHit(c);
+                if (res != HitResponse.Miss)
+                    return res;
+            }
+            return res;
+        }
+
+        public List<Coords> GetCoords()
+        {
+
+        }
 
 
 
